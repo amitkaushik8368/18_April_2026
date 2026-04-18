6139ecbfa0ac16974534051fde6f8e616bf197cc
amitkaushik8368 <amit.kaushik9466@gmail.com>
2026-04-18 22:58:12 +0530

    This is third commit

--- src/main/java/com/selenium/pratima/AdvanceLoginTest.java
+++ src/main/java/com/selenium/pratima/AdvanceLoginTest.java
@@ -1,33 +1,49 @@
 package com.selenium.pratima;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.time.Duration;
 import java.util.Properties;
 
 public class AdvanceLoginTest
 {
-    public static void main(String[] args) throws FileNotFoundException {
-        Properties properties = new Properties();
-        System.out.println(System.getProperty("user.dir"));
-        System.out.println(System.getProperty("user.dir")+"\\src\\main\\resources\\config.properties");//+"\\src\\main\\resources\\config.properties"));
+    public static void main(String[] args) {
+        Properties prop = new Properties();
         try(
                 FileInputStream fis = new FileInputStream
                         (
                                 System.getProperty("user.dir")+"\\src\\main\\resources\\config.properties"
                         )
-        ){
-           properties.load(fis);
-           String username = properties.getProperty("username");
-           String password = properties.getProperty("password");
-           String url = properties.getProperty("url");
-
-           System.out.println("Username for property file: " + username);
-           System.out.println("Password for property file: " + password);
-            System.out.println("Website to be Automated: " + url);
+        )
+        {
+           prop.load(fis);
         }  catch(RuntimeException | IOException e) {
             System.out.println("Exception Occurred");
         }
+        String username = prop.getProperty("username");
+        String password = prop.getProperty("password");
+        String url = prop.getProperty("url");
+        WebDriver driver = new EdgeDriver();
+        driver.manage().window().maximize();
+        driver.get(url);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement usernameLocator = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("username")));
+        WebElement passwordLocator = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("password")));
+        WebElement loginButtonLocator = wait.until(ExpectedConditions.elementToBeClickable(By.className("radius")));
+
+        usernameLocator.sendKeys(username);
+        passwordLocator.sendKeys(password);
+        loginButtonLocator.click();
+
+
 
 
 
