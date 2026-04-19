d7f6b741732c555a469c492401a678d451ce89d4
amitkaushik8368 <amit.kaushik9466@gmail.com>
2026-04-19 13:46:10 +0530

    This is first commit

--- src/main/java/com/selenium/pratima/AdvanceLoginTest.java
+++ src/main/java/com/selenium/pratima/AdvanceLoginTest.java
@@ -8,41 +8,58 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.Properties;
 
 public class AdvanceLoginTest
 {
+    static String validUsername;
+    static String validPassword;
+    static String invalidUsername;
+    static String invalidPassword;
+    static String url;
+    static WebElement usernameLocator;
+    static WebElement passwordLocator;
+    static WebElement loginButton;
+    static WebElement logoutButton;
+    static WebDriver driver;
+    static WebDriverWait wait;
+
     public static void main(String[] args) {
-        Properties prop = new Properties();
-        try(
-                FileInputStream fis = new FileInputStream
-                        (
-                                System.getProperty("user.dir")+"\\src\\main\\resources\\config.properties"
-                        )
-        )
-        {
-           prop.load(fis);
-        }  catch(RuntimeException | IOException e) {
-            System.out.println("Exception Occurred");
-        }
-        String validUsername = prop.getProperty("validUsername");
-        String validPassword = prop.getProperty("validPassword");
-        String invalidUsername = prop.getProperty("invalidUsername");
-        String invalidPassword = prop.getProperty("invalidPassword");
-        String url = prop.getProperty("url");
-        WebDriver driver = new EdgeDriver();
-        driver.manage().window().maximize();
-        driver.get(url);
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement usernameLocator = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("username")));
-        WebElement passwordLocator = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("password")));
-        WebElement loginButtonLocator = wait.until(ExpectedConditions.elementToBeClickable(By.className("radius")));
-        usernameLocator.sendKeys(invalidUsername);
-        passwordLocator.sendKeys(invalidPassword);
-        loginButtonLocator.click();
+        loadProperties();
+        initializeDriver();
+        testValidLogin();
+
+
+
+//        Properties prop = new Properties();
+//        try(
+//                FileInputStream fis = new FileInputStream
+//                        (
+//                                System.getProperty("user.dir")+"\\src\\main\\resources\\config.properties"
+//                        )
+//        )
+//        {
+//           prop.load(fis);
+//        }  catch(RuntimeException | IOException e) {
+//            System.out.println("Exception Occurred");
+//        }
+//        String validUsername = prop.getProperty("validUsername");
+//        String validPassword = prop.getProperty("validPassword");
+//        String invalidUsername = prop.getProperty("invalidUsername");
+//        String invalidPassword = prop.getProperty("invalidPassword");
+//        String url = prop.getProperty("url");
+//        WebDriver driver = new EdgeDriver();
+//        driver.manage().window().maximize();
+//        driver.get(url);
+//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+//        WebElement usernameLocator = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("username")));
+//        WebElement passwordLocator = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("password")));
+//        WebElement loginButtonLocator = wait.until(ExpectedConditions.elementToBeClickable(By.className("radius")));
+//        usernameLocator.sendKeys(invalidUsername);
+//        passwordLocator.sendKeys(invalidPassword);
+//        loginButtonLocator.click();
 
         /**
          *
@@ -61,10 +78,44 @@ public class AdvanceLoginTest
          * Write Results
          *
          */
+    }
 
 
+    static void loadProperties(){
+        Properties prop = new Properties();
+        try(
+                FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "//src//main//resources//config.properties")
+        ) {
+            prop.load(fis);
+        } catch (IOException e) {
+            System.out.println("Exception has come");
+        }
+        validUsername = prop.getProperty("validUsername");
+        validPassword = prop.getProperty("validPassword");
+        invalidUsername = prop.getProperty("invalidUsername");
+        invalidPassword = prop.getProperty("invalidPassword");
+        url = prop.getProperty("url");
 
+    }
 
+    static void initializeDriver()
+    {
+        driver = new EdgeDriver();
+        driver.manage().window().maximize();
+        driver.get(url);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        usernameLocator = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("username")));
+        passwordLocator = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("password")));
+        loginButton = wait.until(ExpectedConditions.elementToBeClickable(By.className("radius")));
+
+    }
+    static void testValidLogin()
+    {
+        usernameLocator.sendKeys(validUsername);
+        passwordLocator.sendKeys(validPassword);
+        loginButton.click();
+        logoutButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[normalize-space()='Logout']")));
+        driver.close();
 
     }
 }
